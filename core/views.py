330c7a5bdf22327f4b7d4fa41cd5692469abf56d
Kiku <kiku.jones@quinnipiac.edu>
2016-01-13 17:59:28 +0000

    Add user authorization

--- core/views.py
+++ core/views.py
@@ -6,6 +6,7 @@ from django.views.generic import DetailView
 from django.views.generic import UpdateView
 from django.views.generic import DeleteView
 from django.core.urlresolvers import reverse_lazy
+from django.core.exceptions import PermissionDenied
 from .models import *
 
 # Create your views here.
@@ -41,11 +42,23 @@ class QuestionUpdateView(UpdateView):
     model = Question
     template_name = 'question/question_form.html'
     fields = ['title', 'description']
+    
+    def get_object(self, *args, **kwargs):
+        object = super(QuestionUpdateView, self).get_object(*args, **kwargs)
+        if object.user != self.request.user:
+            raise PermissionDenied()
+        return object
 
 class QuestionDeleteView(DeleteView):
     model = Question
     template_name = 'question/question_confirm_delete.html'
     success_url = reverse_lazy('question_list')
+    
+    def get_object(self, *args, **kwargs):
+        object = super(QuestionDeleteView, self).get_object(*args, **kwargs)
+        if object.user != self.request.user:
+            raise PermissionDenied()
+        return object
 
 class AnswerCreateView(CreateView):
     model = Answer
@@ -68,6 +81,12 @@ class AnswerUpdateView(UpdateView):
     
     def get_success_url(self):
         return self.object.question.get_absolute_url()
+    
+    def get_object(self, *args, **kwargs):
+        object = super(AnswerUpdateView, self).get_object(*args, **kwargs)
+        if object.user != self.request.user:
+            raise PermissionDenied()
+        return object
         
 class AnswerDeleteView(DeleteView):
     model = Answer
@@ -76,3 +95,9 @@ class AnswerDeleteView(DeleteView):
     
     def get_success_url(self):
         return self.object.question.get_absolute_url()
+        
+    def get_object(self, *args, **kwargs):
+        object = super(AnswerDeleteView, self).get_object(*args, **kwargs)
+        if object.user != self.request.user:
+            raise PermissionDenied()
+        return object
